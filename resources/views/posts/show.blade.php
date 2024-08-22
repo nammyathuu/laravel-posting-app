a4db86deb3600f70bfd9e7a34a58b5e3ab5c4aa0
nammyatyuu <namichan2533@yahoo.co.jp>
2024-08-22 16:33:12 +0900

    変更

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -5,6 +5,9 @@
      <meta charset="UTF-8">
      <meta name="viewport" content="width=device-width, initial-scale=1.0">
      <title>投稿詳細</title>
+
+     {{-- Bootstrap --}}
+     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
  </head>
  
  <body>
@@ -51,6 +54,9 @@
      <footer>
          <p>&copy; 投稿アプリ All rights reserved.</p>
      </footer>
+
+     {{-- Bootstrap --}}
+     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
  </body>
  
  </html>
